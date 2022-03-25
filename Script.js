96b386c6b46e11892ff751400d62cdcbd1aca0ad
Jio Ponda <Jioponda@outlook.fr>
2022-03-25 18:42:04 +0100

    Mise en page + Ajout fonction retour a l'acceuil JS + changement Iframe en img

--- Script.js
+++ Script.js
@@ -1,10 +1,18 @@
-var Lbutton = document.getElementById("Lclick");
-var Rbutton = document.getElementById("Rclick");
+let Lbutton = document.getElementById("Lclick");
+let Rbutton = document.getElementById("Rclick");
+let buttonBack = document.getElementById("Backclick")
 
 function opendoor() {
     document.getElementById("Lpanel").classList.toggle("movepanel-left");
     document.getElementById("Rpanel").classList.toggle("movepanel-right");
 }
 
+function closedoor() {
+    document.getElementById("Lpanel").classList.toggle("movepanel-close-left")
+    document.getElementById("Rpanel").classList.toggle("movepanel-close-right")
+    alert("plop");
+}
+
 Lbutton.addEventListener("click", opendoor);
 Rbutton.addEventListener("click", opendoor);
+buttonBack.addEventListener("click",closedoor);
