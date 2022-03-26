f17f0b4ed39b71f8750158431d09de451a7161c5
Jio Ponda <Jioponda@outlook.fr>
2022-03-26 18:21:41 +0100

    test if + else

--- Script.js
+++ Script.js
@@ -1,6 +1,9 @@
 let Lbutton = document.getElementById("Lclick");
 let Rbutton = document.getElementById("Rclick");
 let buttonBack = document.getElementById("Backclick");
+let Twitch = document.getElementById("Lclick");
+let Youtube = document.getElementById("Rclick");
+
 
 function opendoor() {
     document.getElementById("Lpanel").classList.toggle("movepanel-left");
@@ -8,10 +11,19 @@ function opendoor() {
 }
 
 function closedoor() {
-    document.getElementById("Lpanel").classList.toggle("movepanel-close-left");
-    document.getElementById("Rpanel").classList.toggle("movepanel-close-right");
+    document.getElementById("Lpanel").classList.toggle("movepanel-close-left")
+    document.getElementById("Rpanel").classList.toggle("movepanel-close-right")
 }
+
+function showTwitch(btn) { 
+    if (btn.id == "Lclick") { 
+        console.log("Bonjoir") } 
+    else  {
+        console.log("t'as tout cassé")
+    }
+};
+
 
 Lbutton.addEventListener("click", opendoor);
 Rbutton.addEventListener("click", opendoor);
-buttonBack.addEventListener("click", closedoor);
+buttonBack.addEventListener("click",closedoor);
