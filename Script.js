bc81c437b5fe02bd2112798131e864c8a3572686
Jio Ponda <Jioponda@outlook.fr>
2022-03-29 17:52:38 +0200

    changement JS , ajout onclick , clean des bugs + lien linktree

--- Script.js
+++ Script.js
@@ -1,29 +1,40 @@
 let Lbutton = document.getElementById("Lclick");
 let Rbutton = document.getElementById("Rclick");
 let buttonBack = document.getElementById("Backclick");
-let Twitch = document.getElementById("Lclick");
-let Youtube = document.getElementById("Rclick");
-
 
 function opendoor() {
     document.getElementById("Lpanel").classList.toggle("movepanel-left");
     document.getElementById("Rpanel").classList.toggle("movepanel-right");
-}
+};
 
 function closedoor() {
     document.getElementById("Lpanel").classList.toggle("movepanel-close-left")
     document.getElementById("Rpanel").classList.toggle("movepanel-close-right")
+    setTimeout(function() { 
+        document.getElementById("youtube").style.display = "none";
+        document.getElementById("twitch").style.display = "none";
+    }, 1000);
+};
+
+function clean() {
+    document.getElementById("Lpanel").classList.remove("movepanel-left");
+    document.getElementById("Rpanel").classList.remove("movepanel-right");
+    document.getElementById("Lpanel").classList.remove("movepanel-close-left");
+    document.getElementById("Rpanel").classList.remove("movepanel-close-right");
 }
 
 function showTwitch(btn) { 
     if (btn.id == "Lclick") { 
-        console.log("Bonjoir") } 
-    else  {
-        console.log("t'as tout cassé")
+        document.getElementById("twitch").style.display = "block"; 
+    } else {
+        document.getElementById("youtube").style.display = "none";
     }
 };
 
-
-Lbutton.addEventListener("click", opendoor);
-Rbutton.addEventListener("click", opendoor);
-buttonBack.addEventListener("click",closedoor);
+function showYoutube(btn) {
+    if (btn.id == "Rclick") { 
+        document.getElementById("youtube").style.display = "block"; 
+    } else {
+        document.getElementById("twitch").style.display = "none";
+    }
+};
