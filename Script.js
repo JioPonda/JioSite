4bad0d63127c8b4f04c8d32c0c2ebbcfcd7ea815
Jio Ponda <Jioponda@outlook.fr>
2022-03-30 13:03:26 +0200

    nettoyage dossier Assets et du Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,3 @@
-let Lbutton = document.getElementById("Lclick");
-let Rbutton = document.getElementById("Rclick");
-let buttonBack = document.getElementById("Backclick");
-
 function opendoor() {
     document.getElementById("Lpanel").classList.toggle("movepanel-left");
     document.getElementById("Rpanel").classList.toggle("movepanel-right");
